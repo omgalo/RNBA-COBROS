4ce1a0e8ad10531077c9e71c02da8c2fca2c5027
Mauricio Galo <omgalo@policianacional.gob.hn>
2024-12-06 13:50:15 -0600

    INICIO DE PROYECTO

---
+++ src/main/java/hn/gob/policianacional/dpt/idt/rnba/cobros/CobrosRnbaApplication.java
@@ -0,0 +1,13 @@
+package hn.gob.policianacional.dpt.idt.rnba.cobros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobrosRnbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobrosRnbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/gob/policianacional/dpt/idt/rnba/cobros/CobrosRnbaApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.gob.policianacional.dpt.idt.rnba.cobros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobrosRnbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
